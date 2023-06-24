7b0716836349d063d3447eb3fa0d3a08a5ee12eb
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-24 12:19:39 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_course3/SpringCourse3Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_course3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourse3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourse3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_course3/SpringCourse3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_course3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourse3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
